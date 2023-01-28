d54f16ffc1302fa628a4fdf13b4a0618fd21413b
Megan McBride <meganambermcbride@gmail.com>
2023-01-27 19:33:42 -0500

    Add condition for when cart is empty

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,43 +1,53 @@
 import Navbar from "./Navbar.js";
 import CartItem from "./CartItem.js";
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
 
 function Cart(props) {
+  const { cart, productList } = props;
+  const [filteredCart, setFilteredCart] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
-  //Get cart from local storage and parse data
-  const localStorageCart = window.localStorage.getItem('cart');
-  const parsedLocalStorageCart = JSON.parse(localStorageCart);
-  console.log("----", parsedLocalStorageCart);
-  let cartItems = [];
-
-  //map cart items data into cart item component
-  const getCartItems = () => {
-    if (parsedLocalStorageCart) {
-      cartItems = parsedLocalStorageCart.map((product, index) => {
-
-        return (
-          <CartItem
-            category={product.category}
-            key={index}
-            productId={product.productId}
-            img={product.thumbnail}
-            url={product.url}
-            name={product.name}
-            price={product.price}
-            currency={product.currency}
-          />
-        );
-      });
+  useEffect(() => {
+    if (cart) {
+      setFilteredCart(
+        props.productList.filter((product) => cart.includes(product.productId))
+      );
     }
+  }, [productList]);
 
-    console.log("hello", cartItems);
+
+  useEffect(() => {
+    getCartItems();
+  }, [filteredCart]);
+
+  const getCartItems = () => {
+    if (filteredCart) {
+      setCartItems(
+        filteredCart.map((product, index) => {
+          let itemQuantity = 0;
+          for (let i = 0; i < cart.length; i++) {
+            if (cart[i] === product.productId) {
+              itemQuantity++;
+            }
+          }
+
+          return (
+            <CartItem
+              category={product.category}
+              key={index}
+              productId={product.productId}
+              img={product.thumbnail}
+              url={product.url}
+              name={product.name}
+              price={product.price}
+              currency={product.currency}
+              itemQuantity={itemQuantity}
+            />
+          );
+        })
+      );
+    }
   };
-
-
-
-  getCartItems();
-
 
   return (
     <div>
@@ -45,6 +55,12 @@ function Cart(props) {
 
       {cartItems}
 
+      {cart === null && (
+        <div className="emptyCart">
+          <p>Your cart is empty</p>
+          <button>Shop Now</button>
+        </div>
+      )}
     </div>
   );
 }
